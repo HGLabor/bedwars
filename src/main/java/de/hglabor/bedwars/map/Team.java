4bc4f4d8a50528037ec1c8fb9d582fa91a423933
mooziii <mooz@moozunity.de>
2021-06-11 22:40:31 +0200

    Create Team.java

---
+++ src/main/java/de/hglabor/bedwars/map/Team.java
@@ -0,0 +1,16 @@
+package de.hglabor.bedwars.map;
+
+import java.awt.*;
+
+public class Team {
+
+    private final Color color;
+
+    public Team(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
